1a3140e890a9aed7522d304e9f489ea8d96b5247
Jose <josemanuel21302@gmail.com>
2024-03-05 21:08:20 -0800

    added starter code

--- server.js
+++ server.js
@@ -3,3 +3,17 @@
   this will act like a DNS server
   when the link is opened it would request from our server and return the original URL
 */
+require("dotenv").config()
+const express = require("express")
+const app = express()
+
+app.get("/", (req, res) => {
+  res.json({
+    name: "Jose",
+    age: 22
+  });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log('server is running');
+})
