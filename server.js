536b753ab6e2e2ac2877ad7a5c99d37b4beda2b5
Jose <josemanuel21302@gmail.com>
2024-03-06 23:30:04 -0800

    fixed module error

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
+require("dotenv").config()
+const express = require("express")
+const app = express()
+
 /*
   Goal make a server that returns a shorten Url:
   this will act like a DNS server
   when the link is opened it would request from our server and return the original URL
 */
-require("dotenv").config()
-const express = require("express")
-const app = express()
 
 app.get("/", (req, res) => {
-  res.json({
-    name: "Jose",
-    age: 22
-  });
+  
 });
 
 app.listen(process.env.PORT, () => {
