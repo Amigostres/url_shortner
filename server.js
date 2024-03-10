181c79559c524bd79fb2296bb9488b0c435171f7
Jose <josemanuel21302@gmail.com>
2024-03-09 22:42:57 -0800

    api will now communicate with front end

--- server.js
+++ server.js
@@ -8,8 +8,9 @@ const app = express()
   when the link is opened it would request from our server and return the original URL
 */
 
-app.get("/", (req, res) => {
-  
+app.post("/", (req, res) => {
+  console.log("responded");
+  res.json({name: "hi"})
 });
 
 app.listen(process.env.PORT, () => {
